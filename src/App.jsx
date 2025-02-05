2be1b64b2c4fdd2addf0d1d7369e0a68e9c6b4cd
Shreya <shreya16180@gmail.com>
2025-02-05 19:04:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+	return (
+		<div className="flex px-10 py-24 bg-green-500 text-white text-4xl font-bold">
+			<h1>Whatsaaauuppp</h1>
+		</div>
+	);
+};
+
+export default App;

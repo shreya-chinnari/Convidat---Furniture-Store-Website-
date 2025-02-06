4d55632af2478e2aca8b2e8bec2b5973c774f88a
Shreya <shreya16180@gmail.com>
2025-02-06 12:47:21 +0530

    added sidebar and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,63 @@
 import React from "react";
+import "./index.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/ABout";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Orders from "./pages/Orders";
+import Navbar from "./components/Navbar";
 
 const App = () => {
 	return (
-		<div className="flex px-10 py-24 bg-green-500 text-white text-4xl font-bold">
-			<h1>Whatsaaauuppp</h1>
-		</div>
+		<>
+			<div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+				<Navbar>
+					<Routes>
+						<Route
+							path="/"
+							element={<Home />}
+						/>
+						<Route
+							path="/collection"
+							element={<Collection />}
+						/>
+						<Route
+							path="/about"
+							element={<About />}
+						/>
+						<Route
+							path="/contact"
+							element={<Contact />}
+						/>
+						<Route
+							path="/product/:productId"
+							element={<Product />}
+						/>
+						<Route
+							path="/cart"
+							element={<Cart />}
+						/>
+						<Route
+							path="/login"
+							element={<Login />}
+						/>
+						<Route
+							path="/placeorder"
+							element={<PlaceOrder />}
+						/>
+						<Route
+							path="/orders"
+							element={<Orders />}
+						/>
+					</Routes>
+				</Navbar>
+			</div>
+		</>
 	);
 };
 

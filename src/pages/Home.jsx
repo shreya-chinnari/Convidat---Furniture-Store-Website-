542cd3cdab85ea3c23fbb35523369c1b65c2c41e
Shreya <shreya16180@gmail.com>
2025-02-08 19:01:48 +0530

    Newsletter box .jsx updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Hero from "../components/Hero";
 import LatestCollection from "../components/LatestCollection";
 import BestSeller from "../components/BestSeller";
 import OurPolicy from "../components/OurPolicy";
+import NewsletterBox from "../components/Newsletterbox";
 
 const Home = () => {
 	return (
@@ -12,6 +13,7 @@ const Home = () => {
 			<LatestCollection></LatestCollection>
 			<BestSeller></BestSeller>
 			<OurPolicy></OurPolicy>
+			<NewsletterBox></NewsletterBox>
 		</div>
 	);
 };
